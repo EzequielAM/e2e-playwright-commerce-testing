9e0649cecb6b70d5c3cd8b2f360d991c60f3563c
eze_k <ezequielandresm99@gmail.com>
2026-06-09 20:46:46 -0300

    feat: add visual regression test suite and baseline snapshots for chronium, firefox, and webkit

---
+++ tests/visual.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage');
+
+test.describe('SauceDemo - Visual Regression Testing 🎨', () => {
+
+  test('Validar consistencia visual de la vitrina de productos', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('standard_user', 'secret_sauce');
+    await page.waitForURL('https://www.saucedemo.com/inventory.html');
+
+    await expect(page).toHaveScreenshot('productos-vitrina.png', {
+      fullPage: true,
+      maxDiffPixels: 100,
+    });
+  });
+
+});
